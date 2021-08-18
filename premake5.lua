3a814d09e56101d5b02764d5976b83917aa60694
Nathan Gavieres <86462666+JustARegularPlayer@users.noreply.github.com>
2021-08-18 12:47:15 +0800

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -8,17 +8,22 @@ project "ImGui"
 	files
 	{
 		"imconfig.h",
-    "imgui.h",
-    "imgui.cpp",
-    "imgui_demo.cpp",
-    "imgui_draw.cpp",
-    "imgui_internal.h",
-    "imgui_tables.cpp",
-    "imgui_widgets.cpp",
-    "imstb_rectpack.h",
-    "imstb_textedit.h",
-    "imstb_truetype.h"
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h"
 	}
+
+    defines
+    {
+        "IMGUI_API=__declspec(dllexport)"
+    }
 
 	filter "system:windows"
 		systemversion "latest"
