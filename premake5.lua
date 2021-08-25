8cb736f40217a41bf7fb3931cd5e7f50a6d4ad11
Nathan Gavieres <justaregularplayer000@gmail.com>
2021-08-25 19:14:38 +0800

    Updated premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,34 +1,39 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-
+	cppdialect "C++17"
+	staticruntime "on"
+	
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
+	
 	files
 	{
 		"imconfig.h",
-        "imgui.h",
-        "imgui.cpp",
-        "imgui_demo.cpp",
-        "imgui_draw.cpp",
-        "imgui_internal.h",
-        "imgui_tables.cpp",
-        "imgui_widgets.cpp",
-        "imstb_rectpack.h",
-        "imstb_textedit.h",
-        "imstb_truetype.h"
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h"
 	}
-
-    defines
-    {
-        "IMGUI_API=__declspec(dllexport)"
-    }
-
+	
+	defines
+	{
+		"IMGUI_API=__declspec(dllexport)"
+	}
+	
 	filter "system:windows"
 		systemversion "latest"
-    cppdialect "C++17"
-		staticruntime "on"
+	
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
 
-	filter { "system:windows", "configurations:Release" }
-    buildoptions "MT"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
